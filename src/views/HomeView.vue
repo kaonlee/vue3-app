9c80ffc4894d7f8fe22bc4f14b71de573c6ed30d
Jiaen Lee <nn750137@gmail.com>
2025-06-10 08:41:40 +0800

    feat: initialize Vue 3 project with Vite, TypeScript, and ESLint

---
+++ src/views/HomeView.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts"></script>
+
+<template>
+  <main>
+    <header>
+    <div class="wrapper">
+      <nav>
+        <RouterLink :to="{ name: 'game-mini-city'}">Mini Animal City v.0.1</RouterLink>
+      </nav>
+    </div>
+  </header>
+  </main>
+</template>
---
+++ src/views/MiniCityView.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts"></script>
+
+<template>
+  <main>
+    <header>
+    <h2>Game: Mini Animal City</h2>
+  </header>
+  </main>
+</template>
